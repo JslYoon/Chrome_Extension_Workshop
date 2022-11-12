c207e9a5c5e7c4b29a02aa038571c6eb90e271a8
Kian Boon <39144450+kbbtan@users.noreply.github.com>
2022-11-12 18:39:38 -0500

    Updated to work with MacOS

--- chrome_extension/index.js
+++ chrome_extension/index.js
@@ -1,5 +1,5 @@
 // URL of the Flask Server backend.
-const BACKEND_URL = "http://localhost:5000";
+const BACKEND_URL = "http://127.0.0.1:5000";
 
 // Create a new Todo item.
 const createTodo = async () => {
